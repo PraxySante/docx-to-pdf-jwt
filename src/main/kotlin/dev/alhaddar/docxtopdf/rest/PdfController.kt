69119c7951d29d443d3e225743918297bba93bbc
AngrySquirrell <guigui.maugin@gmail.com>
2025-05-05 11:10:49 +0200

    reimplemented auth.enable that was removed for some reasons

--- src/main/kotlin/dev/alhaddar/docxtopdf/rest/PdfController.kt
+++ src/main/kotlin/dev/alhaddar/docxtopdf/rest/PdfController.kt
@@ -19,7 +19,8 @@ class PdfController(
     val unoService: UnoService,
     @Value("\${auth.secret}") private val authSecret: String,
     @Value("\${auth.publicKey}") private val publicKey: String,
-    @Value("\${auth.algo}") private val algorithm: String
+    @Value("\${auth.algo}") private val algorithm: String,
+    @Value("\${auth.enable:true}") private val authEnable: Boolean
 ) {
     val logger = logger()
     private val verifyJWT = VerifyJWT(
@@ -32,16 +33,25 @@ class PdfController(
     @RequestMapping(value = ["/pdf", "/docx-to-pdf"], method = [RequestMethod.POST])
     fun getPdf(
         @RequestParam("document") file: MultipartFile, 
-        @RequestHeader("Authorization") authorizationHeader: String
+        @RequestHeader(value = "Authorization", required = false) authorizationHeader: String?
     ): ResponseEntity<ByteArray> {
-        val token = authorizationHeader.replace("Bearer ", "")
-        
-        // The verifyToken method now automatically determines the algorithm to use
-        val verificationResult = verifyJWT.verifyToken(token)
-        
-        if (!verificationResult.valid) {
-            logger.warn("JWT verification failed: ${verificationResult.message}")
-            return ResponseEntity.status(verificationResult.code).build()
+        if (authEnable) {
+            if (authorizationHeader == null) {
+                logger.warn("JWT verification failed: Authorization header is missing")
+                return ResponseEntity.status(401).build()
+            }
+            
+            val token = authorizationHeader.replace("Bearer ", "")
+            
+            // The verifyToken method now automatically determines the algorithm to use
+            val verificationResult = verifyJWT.verifyToken(token)
+            
+            if (!verificationResult.valid) {
+                logger.warn("JWT verification failed: ${verificationResult.message}")
+                return ResponseEntity.status(verificationResult.code).build()
+            }
+        } else {
+            logger.info("Auth is disabled, skipping JWT verification")
         }
 
         logger.info("PDF Request. Docx file size: ${file.inputStream.readBytes().size} bytes.")
